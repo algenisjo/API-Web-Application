43037e2ca9fe3c16177601aa0ab376b36ee89a49
Algenis Joaquin Ortiz <algenisjo@Algeniss-Mini.lan>
2021-07-20 12:06:15 -0400

    catch information from api and display it to users

--- index.php
+++ index.php
@@ -1,5 +1,12 @@
 <?php 
 
+// read json
+$json = file_get_contents( 'https://api.coindesk.com/v1/bpi/currentprice.json');
+//decode json
+$jsonArray = json_decode($json, true);
+//print json
+print_r($jsonArray).'<br>';
+
 ?>
 
 <!DOCTYPE html>
@@ -8,16 +15,27 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Instagram Search</title>
+    <title>Bitcoin</title>
 </head>
 <body>
 
-    <div class="form" >
-        <form action="" method="get">
-            <input type="text" placeholder="Enter Location">
-            <button type="submit">Submit</button>
-        </form>
+    <div>
+        <h1>Time</h1>
+        <?php echo $jsonArray['time']['updated']; ?>
     </div>
-    
+
+    <div>
+        <h1>Bitcoin Prices</h1>
+        <h2>USD</h2> 
+        <?php echo '$'.$jsonArray['bpi']['USD']['rate_float']; ?>
+        <h2>Pound</h2> 
+        <?php echo $jsonArray['bpi']['GBP']['rate_float']; ?>
+        <h2>Euro</h2> 
+        <?php echo $jsonArray['bpi']['USD']['rate_float']; ?>
+    </div>
+    <br><br>
+    <?php
+   echo $jsonArray['disclaimer'];
+    ?>
 </body>
 </html>
